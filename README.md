5661a3e5c01a66e9dd8efb2e9d38567b8de0629c
Leonid <leonid.spb2015@yandex.ru>
2022-05-03 18:18:10 +0300

    удаление папки node_modules

--- README.md
+++
@@ -1,1 +0,0 @@
-https://leon-coder.github.io/Daisy/
--- scroll.js
+++ scroll.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
     jQuery("a.scroll").click(function() {
         elementClick = jQuery(this).attr("href")
         destination = jQuery(elementClick).offset().top;
-        jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1100);
+        jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1500);
         return false;
     });
 });
