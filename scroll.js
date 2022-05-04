8e0fce952517dc6f1fbe00a982cbeafb7adfac00
Leonid <leonid.spb2015@yandex.ru>
2022-05-04 17:02:52 +0300

    Проиизведена корректировка стилей для всех разрешений экрана

--- scroll.js
+++ scroll.js
@@ -1,8 +1,12 @@
-jQuery(document).ready(function() {
-    jQuery("a.scroll").click(function() {
-        elementClick = jQuery(this).attr("href")
-        destination = jQuery(elementClick).offset().top;
-        jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1500);
+$(document).ready(function() {
+    $("a.scroll").click(function() {
+        var elementClick = $(this).attr("a.href");
+        var destination = $(elementClick).offset().top;
+        if ($.browser.safari) {
+            $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollTop: destination }, 1100);
+        }
         return false;
     });
 });
